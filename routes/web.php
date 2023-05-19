b0ffe1d9ccfd9141757a4e300450f1f06de96266
Odenilson Marques <odenilson.marques@gmz.solutions>
2023-05-19 20:14:22 -0300

    implementation form of register supplier

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
+use App\Http\Controllers\Supplier\ControllerSupplier;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::get('/suppliers',[ControllerSupplier::class,'create'])->name('supplier.create');
+Route::post('/suppliers',[ControllerSupplier::class,'store'])->name('supplier.store');
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
